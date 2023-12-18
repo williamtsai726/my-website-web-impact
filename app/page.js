bca83dc9258df59f90bf5ec7ad9baa8e9d3c8324
williamtsai726 <williamtsai726@gmail.com>
2023-12-17 17:33:26 -0800

    made navbar

--- app/page.js
+++ app/page.js
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <p>Hellow World!</p>
+  return <Navbar />;
+}
+
+function Navbar() {
+  return (
+    <nav className="border-b sticky top-0 bg-gray-900 text-gray-100 border-gray-800 z-10">
+      <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
+        <a href="/" className="font-medium text-lg md:hover:underline">
+          My Website
+        </a>
+        <ul className="hidden md:flex items-center justify-end space-x-4 text-sm font-medium">
+          <li className="md:hover:underline">
+            <a href="/blog">Blog</a>
+          </li>
+          <li className="md:hover:underline">
+            <a href="/photos">Photos</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
